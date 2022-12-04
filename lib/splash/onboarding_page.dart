9eaf796bbeb723332db754f038300f0c41cb728e
Ardelia Yasmin <ardeliascarlet@gmail.com>
2022-12-04 18:55:27 +0700

    fix onboarding

--- lib/splash/onboarding_page.dart
+++ lib/splash/onboarding_page.dart
@@ -14,42 +14,55 @@ class _OnboardingPageState extends State<OnboardingPage> {
   Widget build(BuildContext context) {
     const bodyStyle = TextStyle(fontSize: 16.0);
 
-    const pageDecoration = const PageDecoration(
+    const pageDecoration = PageDecoration(
       titleTextStyle: TextStyle(fontSize: 24.0, fontWeight: FontWeight.w700),
       bodyTextStyle: bodyStyle,
-      bodyPadding: EdgeInsets.fromLTRB(16.0, 0.0, 16.0, 16.0),
+      bodyPadding: EdgeInsets.fromLTRB(16.0, 0.0, 16.0, 0.0),
       pageColor: Colors.white,
-      imagePadding: EdgeInsets.fromLTRB(16.0, 0.0, 16.0, 16.0),
+      imagePadding: EdgeInsets.fromLTRB(16.0, 0.0, 16.0, 0.0),
+      titlePadding: EdgeInsets.only(bottom: 24.0),
       bodyFlex: 2,
       imageFlex: 3,
+      imageAlignment: Alignment.center,
     );
 
     return IntroductionScreen(
       globalBackgroundColor: Colors.white,
       pages: [
         PageViewModel(
-            title: "1",
-            body: "Contoh",
-            image: Image.asset("assets/onboarding1.png"),
-            decoration: pageDecoration),
+          title: "Belajar Dari Ahlinya",
+          body:
+              "Pembelajaran dimentori oleh para ahli pada bidangnya masing-masing",
+          image: Image.asset("assets/onboarding1.png"),
+          decoration: pageDecoration,
+        ),
         PageViewModel(
-            title: "2",
-            body: "contoh",
-            image: Image.asset("assets/onboarding2.png"),
-            decoration: pageDecoration),
+          title: "Akses Modul Sepuasnya",
+          body: "Modul berlimpah yang bebas diakses dimanapun dan kapanpun",
+          image: Image.asset("assets/onboarding2.png"),
+          decoration: pageDecoration,
+        ),
         PageViewModel(
-          title: "3",
-          body: "contoh",
+          title: "Kelas Bersertifikat",
+          body:
+              "Ikuti kelas, kerjakan tugas dan kuis untuk mendapatkan sertifikat",
           image: Image.asset("assets/onboarding3.png"),
           decoration: pageDecoration,
           footer: ElevatedButton(
             onPressed: () {
-              Navigator.pushReplacement(context,
-                  MaterialPageRoute(builder: (builer) {
-                return WelcomeScreen();
-              }));
+              Navigator.pushReplacement(
+                context,
+                MaterialPageRoute(
+                  builder: (builer) {
+                    return WelcomeScreen();
+                  },
+                ),
+              );
             },
-            child: Text('Get Started'),
+            child: const Text(
+              'Daftar',
+              style: TextStyle(fontSize: 16),
+            ),
           ),
         ),
       ],
@@ -58,21 +71,21 @@ class _OnboardingPageState extends State<OnboardingPage> {
       showNextButton: false,
       showDoneButton: false,
       showBackButton: false,
-      dotsFlex: 3,
-      back: Icon(Icons.arrow_back),
-      skip: Text(
-        'Skip',
+      dotsFlex: 2,
+      back: const Icon(Icons.arrow_back),
+      skip: const Text(
+        'Lewati',
+        style: TextStyle(fontWeight: FontWeight.bold, fontSize: 16),
+      ),
+      next: const Icon(Icons.arrow_forward),
+      done: const Text(
+        'Selesai',
         style: TextStyle(fontWeight: FontWeight.bold),
       ),
-      next: Icon(Icons.arrow_forward),
-      done: Text(
-        'Done',
-        style: TextStyle(fontWeight: FontWeight.bold),
-      ),
-      dotsDecorator: DotsDecorator(
-          size: Size(10, 10),
-          color: Colors.grey,
+      dotsDecorator: const DotsDecorator(
+          size: Size.square(10.0),
           activeSize: Size(22, 10),
+          color: Colors.grey,
           activeShape: RoundedRectangleBorder(
               borderRadius: BorderRadius.all(Radius.circular(35)))),
     );
